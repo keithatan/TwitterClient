c65f608915552714a3de8447120363fe055c0fe3
keithatan <tan230@purdue.edu>
2019-01-28 19:14:53 -0500

    Implemented login call

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onLoginClick(_ sender: Any) {
+        
+        let loginURL = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: loginURL, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print ("Failed")
+        })
+    }
+    
     /*
     // MARK: - Navigation
 
