8d806a56b38625bf069f35f266c6b9becc33a8cb
keithatan <tan230@purdue.edu>
2019-02-04 18:29:59 -0500

    Added TweetViewController

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -15,8 +15,9 @@ class HomeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets();
         self.tweetNum = 20;
+        self.loadTweets();
+        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
 
